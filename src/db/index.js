df5828558e5274f0a2fa6d3af0b07caa2211a676
suryapratap-1 <suryapratap0765@gmail.com>
2023-12-28 11:41:21 +0530

    database connection updated

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-export const connectDB = async () => {
+const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB host: ${connectionInstance.connection.host}`)
